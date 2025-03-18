cc4ad2d6c792932215145217f5edfdd934fd0dbd
APAC\UUJ2KOR <Ujjwal.Kumar@in.bosch.com>
2025-03-18 09:51:40 +0530

    hash map iterator

--- tcsInterviewQuestion/preIncrement.java
+++ tcsInterviewQuestion/preIncrement.java
@@ -10,12 +10,12 @@ public class TernaryTest {
         int a = 2;
         int b = 5;
 
-        int totalPre = a + (b > 6 ? ++a : b--);
-        int totalPost = a + (b > 6 ? ++a : --b);
+        int totalPost = a + (b > 6 ? ++a : b--);
+        int totalPre = a + (b > 6 ? ++a : --b);
         System.out.println(total);
         System.out.println("a: " + a);
         System.out.println("b: " + b);
     }
 }
-//totalPre = 6 (2 + 4), a = 2, b = 4 (In pre decrement first value will be decremented then assigned)
 //totalPost = 7 (2 + 5), a= 2, b = 4 (In post decrement first value will be asigned then decremented)
+//totalPre = 6 (2 + 4), a = 2, b = 4 (In pre decrement first value will be decremented then assigned)
